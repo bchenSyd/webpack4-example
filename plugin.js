0d8ce0e061e87aebb8a5606f72d12cf5e0546c48
Bo Chen <bochen2014@yahoo.com>
2018-05-04 01:37:31 +1000

    update

--- plugin.js
+++ plugin.js
@@ -5,28 +5,34 @@ function ExternalsPlugin(opts) {
   this.opts = opts;
 }
 
-ExternalsPlugin.prototype.apply = function(compiler) {
+ExternalsPlugin.prototype.apply = function (compiler) {
   var opts = this.opts;
-  compiler.plugin('normal-module-factory', function(nmf) {
-    nmf.plugin('factory', function(factory) {
+  compiler.hooks.normalModuleFactory.tap('external plugin', nmf => {
+    nmf.hooks.factory.tap('external factory', factory => {
+      debugger;
+      console.info("=========================================================================");
+      console.info(" my previous plugin's output is now my input ");
+      console.info("=========================================================================");
+
+      return function (data, callback) {
         debugger;
-      return function(data, callback) {
-          debugger;
-        factory(data, function(err, module) {
+        factory(data, function (err, module /* the normal module that native nmf resolved */) {
           if (err) {
             return callback(err);
           }
           if (ModuleFilenameHelpers.matchObject(opts, module.resource)) {
             return callback(null, new ExternalModule(
-              data.dependency.request,
+              data.request,
               opts.type || compiler.options.output.libraryTarget
             ));
           }
           callback(null, module);
         });
       };
+
+
     });
   });
 };
 
-module.exports= ExternalsPlugin;
+module.exports = ExternalsPlugin;
--- reademe.md
+++ reademe.md
@@ -1,3 +1,28 @@
+```
+$ webpack
+=========================================================================
+ my previous plugin's output is now my input
+=========================================================================
+=========================================================================
+ my previous plugin's output is now my input
+=========================================================================
+=========================================================================
+ my previous plugin's output is now my input
+=========================================================================
+Hash: 3e3c2ceb034f29b9b719
+Version: webpack 4.6.0
+Time: 442ms
+Built at: 2018-05-04 01:35:50
+    Asset      Size  Chunks             Chunk Names
+bundle.js  5.99 KiB    main  [emitted]  main
+Entrypoint main = bundle.js
+external "react" 42 bytes {main} [built]
+external "react-dom" 42 bytes {main} [built]
+[./src/index.js] 2.47 KiB {main} [built]
+Done in 0.98s.
+
+```
+
 `npm i babel-core babel-loader babel-preset-env babel-preset-react -D`
 
 ```json
