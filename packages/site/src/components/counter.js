eecb90e41abf41815e65948e98aec5bc291edb3f
Bo Chen <bochen2014@yahoo.com>
2018-06-03 17:44:41 +1000

    update

--- packages/site/src/components/counter.js
+++ packages/site/src/components/counter.js
@@ -2,34 +2,64 @@ import React from "react";
 import rxjsConfig from "recompose/rxjsObservableConfig";
 import {
   componentFromStreamWithConfig,
-  createEventHandlerWithConfig
+  createEventHandlerWithConfig,
+  mapPropsStreamWithConfig,
 } from "recompose";
-import rxjsconfig from "recompose/rxjsObservableConfig";
 
+//symbol-observable doesn't implement rich set of stream transformation methods; so we need to use rxjs instead;
 const componentFromStream = componentFromStreamWithConfig(rxjsConfig);
-const createEventHandler = createEventHandlerWithConfig(rxjsconfig);
+const mapPropsStream = mapPropsStreamWithConfig(rxjsConfig); 
+const createEventHandler = createEventHandlerWithConfig(rxjsConfig);
 
-const Counter = componentFromStream(props$ => {
+const baseComponent = ({ count, increment, decrement, ...rest }) => (
+  <div {...rest}>
+    Count: {count}
+    <button onClick={increment}>+</button>
+    <button onClick={decrement}>-</button>
+  </div>);
+
+
+/*
+class ComponentFromStream extends Component {
+    state = { vdom: null }
+    componendWillMount/ componentWillReceiveProps
+        changeEmitter causes props$ to emit new value
+              ⬇️
+              ⬇️
+              ⬇️
+props$     { name:'bochen' }  --------- {name:'joanna'}   ------------- { name: 'jenny'}  ------
+              ⬇️
+              ⬇️  you can use all forms of transforms, map, combineLatest,merge... anything you like
+              ⬇️
+vdom$    <div>bchen</div>
+              ⬇
+              ⬇
+              ⬇    ️
+render() {
+      return this.state.vdom
+    }
+*/
+
+// stream transform is invoked by Wrapper::CWM and Wrapper::CWR
+const Counter = mapPropsStream(  props$ /*raw props stream: built in Wrapper Component's consturctor*/  => {
   const { handler: increment, stream: increment$ } = createEventHandler();
   const { handler: decrement, stream: decrement$ } = createEventHandler();
   const count$ = increment$.mapTo(1)
     .merge(decrement$.mapTo(-1))
     // merge: similar to combineLatest, but won't combine; 
     // so Observable<T> must merge with another Observable that contains (emits) the same data type
-    .map( (args)=>{
-        return args;
+    .map((args) => {
+      return args;
     })
     .startWith(0)
     // reducer
     .scan((count, n) => count + n, 0);
-
-  return props$.combineLatest(count$, (props, count) => (
-    <div {...props}>
-      Count: {count}
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
-    </div>
-  ));
-});
+  return props$.combineLatest(count$, (props, count) => /*transformed stream*/ ({
+    props,
+    count,
+    increment,
+    decrement
+  }))
+})(baseComponent);
 
 export default Counter;
