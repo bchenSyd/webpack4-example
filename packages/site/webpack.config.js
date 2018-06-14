0e2f632fa14ac3cb3ceb10d1a0a44c9cd2ddc1bd
Bo Chen <bo.chen@westpac.com.au>
2018-06-14 16:28:14 +1000

    update

--- packages/site/webpack.config.js
+++ packages/site/webpack.config.js
@@ -1,10 +1,10 @@
 const path = require("path");
 const chalk = require("chalk");
-const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const webpack = require("webpack");
+const autoprefixer = require("autoprefixer");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const ExternalsPlugin = require("./plugin");
 
 module.exports = {
@@ -30,41 +30,46 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           "css-loader",
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
               plugins: [
                 autoprefixer({
-                  browsers: ['last 2 versions']
+                  browsers: ["last 2 versions"]
                 })
               ]
             }
           },
-          "less-loader"]
+          "less-loader"
+        ]
       },
       {
         test: /\.(jpe?g|gif|png|svg)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            fallback: 'file-loader',
-            limit: 10000,
-            minetype: 'image/svg+xml',
-          },
-        }],
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              fallback: "file-loader",
+              limit: 10000,
+              minetype: "image/svg+xml"
+            }
+          }
+        ]
       },
       {
         test: /\.(ico)|((ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9]))|(ttf|eot)$/i,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]"
+            }
           }
-        }]
-      },
+        ]
+      }
     ]
   },
   externals: [
-    function (context, request, callback) {
+    function(context, request, callback) {
       // console.log(chalk.blue(' got a module request: '+ request, 'context: '+ context))
       if (/^(\.|\/|\!)$/.test(request)) {
         // internal files;
@@ -84,16 +89,21 @@ module.exports = {
       chunkFilename: "[id].css"
     }),
     new HtmlWebpackPlugin({
-      title: 'recompose example'
+      title: "recompose example",
+      meta: {
+        viewport: "width=device-width, initial-scale=1, shrink-to-fit=no"
+      }
     }),
     new webpack.HotModuleReplacementPlugin(), //required when run from wds cli
-    new CopyWebpackPlugin([{
-      from:'greenId-internal',
-      to:'greenid'
-    }])
+    new CopyWebpackPlugin([
+      {
+        from: "greenId-internal",
+        to: "greenid"
+      }
+    ])
   ],
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.less']
+    extensions: [".js", ".jsx", ".json", ".less"]
   },
   devServer: {
     disableHostCheck: true,
@@ -102,10 +112,10 @@ module.exports = {
     historyApiFallback: true,
 
     //#######################################################################
-    //> https://github.com/webpack/webpack/issues/1151#issuecomment-343800515 
-    hot: true, // if you set `hot` to true within `devServer` section and run `webpack-dev-server` cli, you must
-    // either `new webpack.HotModuleReplacementPlugin()` 
-    // or, 
+    //> https://github.com/webpack/webpack/issues/1151#issuecomment-343800515
+    hot: true // if you set `hot` to true within `devServer` section and run `webpack-dev-server` cli, you must
+    // either `new webpack.HotModuleReplacementPlugin()`
+    // or,
     // put `--hot` in cli options (which instruct wds to auto insert a HMRPlugin into your webpac.config.js)
     //#######################################################################
   }
